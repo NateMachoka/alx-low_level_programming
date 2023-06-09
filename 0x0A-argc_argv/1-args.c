620086601a2cf88bf6503138b7629f32fc45dd5d
NateMachoka <natemachoka@gmail.com>
2023-06-09 03:29:58 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments passed through the program
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int num_args = argc -1;
+
+	printf("%d\n", num_args);
+	return (0);
+}
