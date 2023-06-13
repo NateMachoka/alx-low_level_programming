81f61d84a9c884c9552d8c2362f9292c8f8d3d2e
NateMachoka <natemachoka@gmail.com>
2023-06-13 10:12:39 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - converts arguments paseed in the program to strings
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: ...
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int total_length = 0;
+	int i, j, k;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			total_length++;
+		}
+		total_length++;
+	}
+	result = (char *)malloc((total_length + ac + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[k] = av[i][j];
+			k++;
+		}
+		result[k] = '\n';
+		k++;
+	}
+	result[k] = '\0';
+
+	return (result);
+}
