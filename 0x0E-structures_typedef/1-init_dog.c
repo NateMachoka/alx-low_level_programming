9bc2ba98c30af6dc489ba0c799912556fbf9df48
NateMachoka <natemachoka@gmail.com>
2023-06-19 06:09:58 -0700

    changes made to print_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,23 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int i;
-	int name_length = 0;
-	int owner_length = 0;
-
-	while (name[name_length] != '\0')
-		name_length++;
-	while (owner[owner_length] != '\0')
-		owner_length++;
-
-	d->name = (char *)malloc((name_length + 1) * sizeof(char));
-	d->owner = (char *)malloc((owner_length + 1) * sizeof(char));
-
-	for (i = 0; i <= name_length; i++)
-		d->name[i] = name[i];
-
-	for (i = 0; i <= owner_length; i++)
-		d->owner[i] = owner[i];
-
-	d->age = age;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
