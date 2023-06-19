85158289b12c5683e518a2f07be4d3332dd8d868
NateMachoka <natemachoka@gmail.com>
2023-06-19 05:29:16 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@
   * Return: Nothing
   */
 
-void print_dog(struct dog* d)
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
@@ -19,4 +19,5 @@ void print_dog(struct dog* d)
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %.1f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	;
 }
