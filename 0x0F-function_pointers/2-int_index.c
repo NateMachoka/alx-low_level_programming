b60b9d03ff164d853af25d546f8ceea8e74b5ff3
NateMachoka <natemachoka@gmail.com>
2023-06-22 03:48:22 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,21 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return i;
+			return (i);
 	}
 
 	return (-1);
+}
+
+int compare(int value)
+{
+	if (value % 2 == 0)
+		return (0);
+	else
+		return (1);
 }
