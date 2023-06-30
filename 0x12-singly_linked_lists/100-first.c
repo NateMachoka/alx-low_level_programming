e804a018650b7c083bfaed2a2c4c8a4046f62d14
NateMachoka <natemachoka@gmail.com>
2023-06-30 13:38:37 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+void printStatement (void) __attribute__ ((constructor));
+/**
+  * printStatement - prints statements before main function is printed
+  *
+  * Return: Nothing.
+  */
+
+void printStatement(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
