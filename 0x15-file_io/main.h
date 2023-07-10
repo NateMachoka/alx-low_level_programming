c5488621f2b0c11b78581edc6063ab492fde2500
NateMachoka <natemachoka@gmail.com>
2023-07-10 05:38:22 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef _FILE_IO_
 #define _FILE_IO_
 
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -9,7 +10,7 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *s);
+int _strlen(const char *str);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
