f0e8a3a4a9cbad08f565c13d820fc49b13d5a473
NateMachoka <natemachoka@gmail.com>
2023-07-10 07:43:40 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(const char *str);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(const char *src, const char *dest);
+void exitWithError(const char *message, const void *arg);
+int openSourceFile(const char *fileFrom);
+int openDestinationFile(const char *fileTo);
+void copyFileContent(int fdFrom, int fdTo);
+int main(int argc, char *argv[]);
 int _putchar(char c);
 
 #endif
